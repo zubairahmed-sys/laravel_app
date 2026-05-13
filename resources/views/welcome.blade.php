4c0050e7a82dc586596581112f0669bf5fe27d58
zubairahmed-sys <zubair.ahmed@jumppace.com>
2026-05-14 04:49:12 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,10 +48,10 @@
                                     </svg>
                                 </div>
 
-                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Jumppace</h2>
+                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Jumppace PVT LTD</h2>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                    Zubair_Ahmed_Khan testing_laravel_app today 14 May 2026. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
+                                    Zubair Ahmed Khan testing_laravel_app today 14 May 2026. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
                                 </p>
                             </div>
 
